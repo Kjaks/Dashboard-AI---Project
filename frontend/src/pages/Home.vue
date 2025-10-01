c691ef470434375ba4e7eec0d9b788d604083031
kjaks <karolisjakas04@gmail.com>
2025-10-01 22:51:30 +0200

    configuring the front

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Bienvenido a la app 🚀</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
